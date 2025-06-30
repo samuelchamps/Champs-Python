1d07c99eb7e1699f282fe009270582091e4f7f44
samuelchamps <144665489+samuelchamps@users.noreply.github.com>
2025-06-30 15:46:43 +0100

    Add files via upload
    Login system.

---
+++ Login program.py
@@ -0,0 +1,13 @@
+print("Login Programme!")
+print()
+
+username = input("Username: ")
+password = input("Password: ")
+print()
+
+if username == "Champs" and password == "##ChAmpS911":
+    print(f"Welcome back, {username}")
+elif username == "Samuel" and password == "##SamUel3737":
+    print(f"Welcome back, {username}")
+else:
+    print(f"Invalid credentials.")
